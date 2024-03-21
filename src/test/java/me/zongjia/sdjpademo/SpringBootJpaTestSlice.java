3c3a5533eab080f1993d8b3e4cddcaf37f6ee39e
ZONG-JIA GU <kabuto412rock@gmail.com>
2024-03-21 18:25:11 +0800

    Add test cases demonstrating execution order and Transaction mechanism(Not Recommend for most time)
    - Added unit tests to demonstrate the execution order of certain functions
    - Included tests to validate the Transaction mechanism
    - Ensured all tests pass successfully

--- src/test/java/me/zongjia/sdjpademo/SpringBootJpaTestSlice.java
+++ src/test/java/me/zongjia/sdjpademo/SpringBootJpaTestSlice.java
@@ -2,26 +2,42 @@ package me.zongjia.sdjpademo;
 
 import me.zongjia.sdjpademo.domain.Book;
 import me.zongjia.sdjpademo.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
 
     @Autowired
     BookRepository bookRepository;
 
+    @Commit
+    @Order(1)
     @Test
     void testJpaTestSlice() {
         long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(0);
 
         bookRepository.save(new Book("My BOok", "1234555", "Self"));
 
         long countAfter = bookRepository.count();
 
         assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
     }
 }
