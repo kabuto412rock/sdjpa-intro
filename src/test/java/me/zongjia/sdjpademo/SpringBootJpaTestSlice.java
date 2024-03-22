b0642bcff73c841911debb5aa01a2987486556e2
ZONG-JIA GU <kabuto412rock@gmail.com>
2024-03-22 16:04:34 +0800

    BootStrap Data with `@ComponentScan`

--- src/test/java/me/zongjia/sdjpademo/SpringBootJpaTestSlice.java
+++ src/test/java/me/zongjia/sdjpademo/SpringBootJpaTestSlice.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 
@@ -15,6 +16,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
+@ComponentScan(basePackages = {"me.zongjia.sdjpademo.bootstrap"})
 public class SpringBootJpaTestSlice {
 
     @Autowired
@@ -25,7 +27,7 @@ public class SpringBootJpaTestSlice {
     @Test
     void testJpaTestSlice() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(0);
+        assertThat(countBefore).isEqualTo(2);
 
         bookRepository.save(new Book("My BOok", "1234555", "Self"));
 
@@ -38,6 +40,6 @@ public class SpringBootJpaTestSlice {
     @Test
     void testJpaTestSliceTransaction() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(1);
+        assertThat(countBefore).isEqualTo(3);
     }
 }
